8a18dd6cc2a2ec825d0f75254615cd6ff9dd86b4
muhanifrafi <muhanifrafi@gmail.com>
2023-11-10 13:46:22 +0700

    first commit

---
+++ tugas8kel26/Form1.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(345, 151)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(192, 230)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 1
+        Button1.Text = "C to R"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(192, 259)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 2
+        Button2.Text = "C to F"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(192, 288)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 3
+        Button3.Text = "C to K"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(286, 288)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 23)
+        Button4.TabIndex = 6
+        Button4.Text = "R to K"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(286, 259)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(75, 23)
+        Button5.TabIndex = 5
+        Button5.Text = "R to F"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(286, 230)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(75, 23)
+        Button6.TabIndex = 4
+        Button6.Text = "R to C"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(478, 288)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(75, 23)
+        Button7.TabIndex = 12
+        Button7.Text = "K to F"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(478, 259)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(75, 23)
+        Button8.TabIndex = 11
+        Button8.Text = "K to R"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(478, 230)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(75, 23)
+        Button9.TabIndex = 10
+        Button9.Text = "K to C"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Location = New Point(384, 288)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(75, 23)
+        Button10.TabIndex = 9
+        Button10.Text = "F to K"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Location = New Point(384, 259)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(75, 23)
+        Button11.TabIndex = 8
+        Button11.Text = "F to R"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.Location = New Point(384, 230)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(75, 23)
+        Button12.TabIndex = 7
+        Button12.Text = "F to C"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(451, 155)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 15)
+        Label1.TabIndex = 13
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label1)
+        Controls.Add(Button7)
+        Controls.Add(Button8)
+        Controls.Add(Button9)
+        Controls.Add(Button10)
+        Controls.Add(Button11)
+        Controls.Add(Button12)
+        Controls.Add(Button4)
+        Controls.Add(Button5)
+        Controls.Add(Button6)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ tugas8kel26/Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports TempConv
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim objConv As New TempConvComp()
+        Dim v As Double
+        v = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.cToR(v)
+        Label1.Text = "Reamur"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim objConv As New TempConvComp()
+        Dim v As Double
+        v = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.cToF(v)
+        Label1.Text = "Fahrenheit"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim objConv As New TempConvComp()
+        Dim v As Double
+        v = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.cToK(v)
+        Label1.Text = "Kelvin"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim objConv As New TempConvComp()
+        Dim v As Double
+        v = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.rToC(v)
+        Label1.Text = "Celsius"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim objConv As New TempConvComp()
+        Dim v As Double
+        v = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.rToF(v)
+        Label1.Text = "Fahrenheit"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim objConv As New TempConvComp()
+        Dim v As Double
+        v = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.rToK(v)
+        Label1.Text = "Kelvin"
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Dim objConv As New TempConvComp()
+        Dim v As Double
+        v = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.fToC(v)
+        Label1.Text = "Celsius"
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Dim objConv As New TempConvComp()
+        Dim v As Double
+        v = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.fToR(v)
+        Label1.Text = "Reamur"
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Dim objConv As New TempConvComp()
+        Dim v As Double
+        v = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.fToK(v)
+        Label1.Text = "Kelvin"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim objConv As New TempConvComp()
+        Dim v As Double
+        v = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.kToC(v)
+        Label1.Text = "Celsius"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim objConv As New TempConvComp()
+        Dim v As Double
+        v = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.kToR(v)
+        Label1.Text = "Reamur"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim objConv As New TempConvComp()
+        Dim v As Double
+        v = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.kToF(v)
+        Label1.Text = "Fahrenheit"
+    End Sub
+End Class
